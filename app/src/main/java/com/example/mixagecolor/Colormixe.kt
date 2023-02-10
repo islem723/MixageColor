c8e3705d240a292b883e58d09c88ddbee604478b
islem naffeti <islem.naffeti@esprit.tn>
2023-02-10 12:49:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mixagecolor/Colormixe.kt
@@ -0,0 +1,11 @@
+package com.example.mixagecolor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Colormixe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_colormixe)
+    }
+}
